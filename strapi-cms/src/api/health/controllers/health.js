347b12ad3af7f365f7ae9a9446b8960917cce8e3
Alohadancemeow <nawongsak@gmail.com>
2024-06-29 16:45:55 +0700

    🐛 fixed: health-check

--- strapi-cms/src/api/health/controllers/health.js
+++ strapi-cms/src/api/health/controllers/health.js
@@ -1,7 +1,15 @@
+'use strict';
+
+/**
+ * A set of functions called "actions" for `health`
+ */
+
 module.exports = {
-    async index(ctx) {
-        ctx.send({
-            status: 'ok'
-        });
-    },
+  // exampleAction: async (ctx, next) => {
+  //   try {
+  //     ctx.body = 'ok';
+  //   } catch (err) {
+  //     ctx.body = err;
+  //   }
+  // }
 };
